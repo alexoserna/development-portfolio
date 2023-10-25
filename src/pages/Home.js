439268744d6e4407524d89f42e3a5db69094caff
alexoserna <118146045+alexoserna@users.noreply.github.com>
2023-10-24 21:06:51 -0400

    started customizing elemnts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
 export default function Home () {
     return (
-        <h1>Welcome Home</h1>
+        <div>
+            <h1>Welcome Home</h1>
+            <div className="card" >
+                <h2>Alexis</h2>
+                <div className="profile-img">
+                    <img src=''/>
+                </div>
+                
+
+            </div>
+        </div>
     );
 }
