97af7b3df8af3e47208720a046366480fea423b7
alexoserna <118146045+alexoserna@users.noreply.github.com>
2024-02-12 22:27:04 -0500

    restarting from scratch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,10 @@
+import React from "react";
+
 export default function Home () {
     return (
         <div>
             <h1>Welcome Home</h1>
-            <div className="card" >
-                <h2>Alexis</h2>
-                <div className="profile-img">
-                    <img src=''/>
-                </div>
-                
 
-            </div>
         </div>
     );
 }
